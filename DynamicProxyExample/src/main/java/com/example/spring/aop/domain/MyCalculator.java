d7454259e4f849f4da4dedbe063d7ec4dbf6c4b8
Daehyeok Kim <yeriel9159@gmail.com>
2017-11-28 22:48:27 +0900

    * java.lang.reflect의 Proxy.newProxyInstance를 통한 Dynamic Proxy Object 생성

---
+++ DynamicProxyExample/src/main/java/com/example/spring/aop/domain/MyCalculator.java
@@ -0,0 +1,26 @@
+package com.example.spring.aop.domain;
+
+public class MyCalculator implements Calculator 
+{
+
+	public int add(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	public int subtract(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	public int multiply(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	public int divide(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+}
