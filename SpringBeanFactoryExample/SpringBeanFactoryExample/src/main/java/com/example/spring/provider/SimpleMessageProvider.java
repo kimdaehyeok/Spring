da945d336260f7ebe2ae5f1e462e542b8e8bead7
kdh <kdh@gmail.com>
2018-12-30 12:45:42 +0900

    fixed untracked files

---
+++ SpringBeanFactoryExample/SpringBeanFactoryExample/src/main/java/com/example/spring/provider/SimpleMessageProvider.java
@@ -0,0 +1,10 @@
+package com.example.spring.provider;
+
+public class SimpleMessageProvider implements IMessageProvider
+{
+	public String getMessage() 
+	{
+		// TODO Auto-generated method stub
+		return "Hello BeanFactory";
+	}
+}
